8ad34f68bbe657c904144419f6bc044c44e12c94
kolegran <kolegran@gmail.com>
2019-01-04 16:44:13 +0200

    Implemented HTML support for email message

--- src/main/java/com/github/kolegran/trackdebtsemailservice/SmptExample.java
+++ src/main/java/com/github/kolegran/trackdebtsemailservice/SmptExample.java
@@ -1,21 +1,23 @@
 package com.github.kolegran.trackdebtsemailservice;
 
 import org.apache.commons.mail.*;
+import org.apache.commons.mail.HtmlEmail;
 
 public class SmptExample {
 
     public static void main(String[] args) throws Exception {
-        Email email = new SimpleEmail();
+        HtmlEmail email = new HtmlEmail();
 
         email.setHostName("smtp.gmail.com");
         email.setSmtpPort(587);
         email.setAuthenticator(new DefaultAuthenticator("smtp.test.kolegran@gmail.com", "1qazXsw2"));
         email.setSSL(true);
 
-        email.setFrom("smtp.test.kolegran@gmail.com");
+        email.setFrom("smtp.test.kolegran@gmail.com", "Sender");
+        email.addTo("user@gmail.com", "Recipient");
         email.setSubject("TestMail");
-        email.setMsg("Contains some text");
-        email.addTo("user@gmail.com");
+        email.setHtmlMsg("<html>Contains some message</html>");
+        email.setTextMsg("Contains some message");
 
         email.send();
 
