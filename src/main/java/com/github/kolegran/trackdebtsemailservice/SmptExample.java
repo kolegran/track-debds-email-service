80d1a64bd5ce2ab0aeed3086190c4b573056dddd
kolegran <kolegran@gmail.com>
2019-01-07 01:59:48 +0200

    Implemented a method for loading an image from a file

--- src/main/java/com/github/kolegran/trackdebtsemailservice/SmptExample.java
+++ src/main/java/com/github/kolegran/trackdebtsemailservice/SmptExample.java
@@ -1,7 +1,13 @@
 package com.github.kolegran.trackdebtsemailservice;
 
+
 import org.apache.commons.mail.*;
 import org.apache.commons.mail.HtmlEmail;
+
+import java.io.*;
+import javax.activation.DataSource;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class SmptExample {
 
@@ -15,12 +21,21 @@ public class SmptExample {
 
         email.setFrom("smtp.test.kolegran@gmail.com", "Sender");
         email.addTo("user@gmail.com", "Recipient");
+
+        String image = email.embed(resourceImageToDataSource("img.jpg"), "img");
+
         email.setSubject("TestMail");
-        email.setHtmlMsg("<html>Contains some message</html>");
+        email.setHtmlMsg("<html><body><img src=\"cid:" + image + "\"/></body></html>");
         email.setTextMsg("Contains some message");
 
         email.send();
 
         System.out.println("Sending email");
+    }
+
+    private static DataSource resourceImageToDataSource(String resourceImageName) throws IOException {
+        InputStream is = SmptExample.class.getClassLoader().getResourceAsStream(resourceImageName);
+        String contentType = Files.probeContentType(Path.of(resourceImageName));
+        return new ByteArrayDataSource(new BufferedInputStream(is).readAllBytes(), contentType);
     }
 }
