312a7f3e46f4c8c5548d760d9d86f3527495ddda
kolegran <kolegran@gmail.com>
2019-01-09 20:12:53 +0200

    Added getting of the users ledger data from the database
    Added email view form

--- src/main/java/com/github/kolegran/trackdebtsemailservice/SmptExample.java
+++ src/main/java/com/github/kolegran/trackdebtsemailservice/SmptExample.java
@@ -1,17 +1,15 @@
 package com.github.kolegran.trackdebtsemailservice;
 
-
 import org.apache.commons.mail.*;
 import org.apache.commons.mail.HtmlEmail;
-
-import java.io.*;
-import javax.activation.DataSource;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class SmptExample {
 
     public static void main(String[] args) throws Exception {
+        // TODO
+    }
+
+    public static void emailSender(String info) throws Exception {
         HtmlEmail email = new HtmlEmail();
 
         email.setHostName("smtp.gmail.com");
@@ -20,22 +18,14 @@ public class SmptExample {
         email.setSSL(true);
 
         email.setFrom("smtp.test.kolegran@gmail.com", "Sender");
-        email.addTo("user@gmail.com", "Recipient");
-
-        String image = email.embed(resourceImageToDataSource("img.jpg"), "img");
+        email.addTo("kolegran@gmail.com", "Recipient");
 
         email.setSubject("TestMail");
-        email.setHtmlMsg("<html><body><img src=\"cid:" + image + "\"/></body></html>");
+        email.setHtmlMsg("<html><body></body></html>");
         email.setTextMsg("Contains some message");
 
         email.send();
 
         System.out.println("Sending email");
-    }
-
-    private static DataSource resourceImageToDataSource(String resourceImageName) throws IOException {
-        InputStream is = SmptExample.class.getClassLoader().getResourceAsStream(resourceImageName);
-        String contentType = Files.probeContentType(Path.of(resourceImageName));
-        return new ByteArrayDataSource(new BufferedInputStream(is).readAllBytes(), contentType);
     }
 }
--- src/main/java/com/github/kolegran/trackdebtsemailservice/TrackDebtsEmailServiceApplication.java
+++ src/main/java/com/github/kolegran/trackdebtsemailservice/TrackDebtsEmailServiceApplication.java
@@ -1,11 +1,13 @@
 package com.github.kolegran.trackdebtsemailservice;
 
-
 import org.postgresql.ds.PGSimpleDataSource;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.*;
+
 
 public class TrackDebtsEmailServiceApplication {
 
@@ -17,14 +19,125 @@ public class TrackDebtsEmailServiceApplication {
         dataSource.setPassword("postgres");
         dataSource.setDatabaseName("track-debts");
         Connection connection = dataSource.getConnection();
-        ResultSet resultSet = connection.createStatement().executeQuery("select * from users");
 
-        while(resultSet.next()) {
-            System.out.println(resultSet.getLong("id"));
-            System.out.println(resultSet.getString("email"));
-            System.out.println();
+        List<UserLedger> userLedgers = getUserLedgers(connection);
+
+        Set<UserLedger> victim = new HashSet<>();
+
+        for (UserLedger userLedger : userLedgers) {
+            for (UserBalance userBalance : userLedger.userBalanceList) {
+                if (userBalance.amount.compareTo(new BigDecimal(-80)) < 0) {
+                    victim.add(userLedger);
+                }
+            }
+        }
+
+        for (UserLedger userLedger : victim) {
+            sendEmail(userLedger);
+
         }
         connection.close();
     }
+
+    private static void sendEmail(UserLedger userLedger) {
+        try {
+            SmptExample.emailSender(userLedger.description());
+        } catch (Exception e)
+        {
+            System.err.println("Error");
+        }
+    }
+
+    private static List<UserLedger> getUserLedgers(Connection connection) throws SQLException {
+        ArrayList<UserLedger> res = new ArrayList<>();
+
+        ResultSet userRs = connection.createStatement().executeQuery("select * from users order by id");
+
+        while (userRs.next()) {
+            UserLedger userLedger = new UserLedger();
+            userLedger.userId = userRs.getLong("id");
+            userLedger.fullName = userRs.getString("first_name") + " " + userRs.getString("last_name");
+            Map<Long, BigDecimal> sumPerUser = getLedger(connection, userRs.getLong("id"));
+
+            for (Long userId : sumPerUser.keySet()) {
+                UserBalance userBalance = new UserBalance();
+                userBalance.userId = userId;
+                userBalance.fullName = getFullNameByUserId(connection, userId);
+                userBalance.amount = sumPerUser.get(userId);
+                userLedger.userBalanceList.add(userBalance);
+            }
+
+            res.add(userLedger);
+        }
+        return res;
+    }
+
+    private static String getFullNameByUserId(Connection connection, Long userId)  throws SQLException {
+        ResultSet result = connection.createStatement().executeQuery("select * from users where id = " + userId + "order by id");
+
+        result.next();
+
+        return result.getString("first_name") + " " + result.getString("last_name");
+    }
+
+    private static Map<Long, BigDecimal> getLedger(Connection connection, long userId) throws SQLException {
+        ResultSet senderRs = connection.createStatement().executeQuery(
+                "select * from money_transaction where sender_id = " + userId + " or receiver_id = " + userId
+        );
+
+        Map<Long, BigDecimal> sumPerUser = new HashMap<>();
+
+        while (senderRs.next()) {
+            long receiverId = senderRs.getLong("receiver_id");
+            long senderId = senderRs.getLong("sender_id");
+
+            if (userId == senderId) {
+                BigDecimal sum = sumPerUser.getOrDefault(receiverId, BigDecimal.ZERO);
+                sumPerUser.put(receiverId, sum.add(senderRs.getBigDecimal("amount")));
+            } else {
+                BigDecimal sum = sumPerUser.getOrDefault(senderId, BigDecimal.ZERO);
+                sumPerUser.put(senderId, sum.subtract(senderRs.getBigDecimal("amount")));
+            }
+        }
+
+        return sumPerUser;
+    }
+
 }
 
+class UserLedger {
+    Long userId;
+    String fullName;
+    List<UserBalance> userBalanceList = new ArrayList<>();
+
+    BigDecimal totalDebt() {
+        BigDecimal s = BigDecimal.ZERO;
+        for (UserBalance userBalance : userBalanceList) {
+            if (userBalance.amount.compareTo(BigDecimal.ZERO) < 0) {
+                s = s.add(userBalance.amount);
+            }
+        }
+        return s.multiply(new BigDecimal(-1));
+    }
+
+    public String description() {
+        /*System.out.println("User id is: " + userId);
+        System.out.println("User full name: " + fullName);
+
+        for (UserBalance u : userBalanceList) {
+            System.out.println("You are the debtor of " + u.fullName + ". The debt is: " + u.amount);
+        }
+
+        System.out.println("Total debt is: " + totalDebt());
+        System.out.println();*/
+
+        // for test
+        return userId.toString();
+    }
+}
+
+class UserBalance {
+    Long userId;
+    String fullName;
+    BigDecimal amount;
+}
